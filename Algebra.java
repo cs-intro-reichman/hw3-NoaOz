e5abfeb2a461aafb7fc0fb64ec9ef46668f35542
NoaOz <noa.oz01@post.runi.ac.il>
2024-11-27 23:43:35 +0200

    completed 1

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,124 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int variable1 = x1; 
+		int variable2 = x2; 
+		for (int i = 0; i < variable2; i++ ){
+			variable1 ++;
+		}
+		return variable1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int variable1 = x1; 
+		int variable2 = x2; 
+		for (int i = 0; i < variable2; i++){
+			variable1 --;
+		}
+		return variable1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int variable1 = x1; 
+		int variable2 = x2;
+		int multiplication = x1;
+		if (variable1 == 1){
+			return variable2;
+		}
+			if (variable1 == 0){
+				return 0; 
+			}
+				if (variable2 == 0) {
+					return 0; 
+				}
+					else {
+						for (int i = 1; i < variable2; i++) {
+							multiplication = plus(multiplication, variable1); 
+						} 	
+							return multiplication;
+					}
+
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int variable = x ;
+		int num = n ;
+		int result = x; 
+		if (num == 1){
+			return variable;
+		}
+			if (num == 0){
+				return 1;
+			}
+				else{
+					for (int i = 1; i < num; i++){
+						result = times(result, variable);
+					}
+					return result;
+
+				}
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int variable1 = x1;
+		int variable2 = x2;
+		int counter = 0; 
+		if (variable1 == variable2){
+			return 1;
+		} 
+			if (variable1 < variable2){
+				return 0; 
+			}
+				if (variable2 == 1){
+					return variable1;
+				}
+					else {
+
+						while (variable1 > 0){
+							variable1 = minus(variable1, variable2);
+							if (variable1 >= 0){
+								counter ++; 
+							}
+						}
+						return counter;
+				}
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int variable1 = x1;
+		int variable2 = x2; 
+		if (variable1 == variable2){
+			return 0;
+		}
+			if ( variable2 > variable1){
+				return variable1;
+			}
+				else { 
+					int modular = div(variable1, variable2);
+					modular = times(modular, variable2);
+					modular = minus(variable1, modular);
+					return modular;
+
+				}
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
+		int variable = x; 
+		if (x > 0){
+			int i = 1; 
+			while (i <= variable){
+				if (variable == pow(i,2)) {
+					return i;
+				}
+				i++ ;
+			}
+		}
 		return 0;
 	}	  	  
 }
