9b329dfb709d2feb78ea37fa1bbc0500ca758d4e
NoaOz <noa.oz01@post.runi.ac.il>
2024-11-28 18:08:10 +0200

    completed Algebra

--- Algebra.java
+++ Algebra.java
@@ -22,138 +22,181 @@ public class Algebra {
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
 	}  
+	// Returns the absolute value
+	public static int absolute(int x1) {
+		int positive = 0;
+		if (x1 >= 0) {
+			return x1; 
+		}
+			else {
+				for (int i = x1; i < 0; x1++){
+				positive++;
+			}
+			return positive;
+		}
+		 
+	}
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		int variable1 = x1; 
-		int variable2 = x2; 
-		if (variable2 >= 0){
-			for (int i = 0; i < variable2; i++ ){
-			variable1 ++;
+		for (int i = 0; i < absolute(x2); i++) {
+			if (x2 < 0) {
+				x1-- ;
+			}
+			else {
+				x1++ ;
+			}
 		}
-	}
-				else {
-				variable1 = minus(variable1, variable2);	
-				}
-
-		return variable1;
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		int variable1 = x1; 
-		int variable2 = x2; 
-		if (variable2 >= 0){
-			for (int i = 0; i < variable2; i++){
-			variable1 --;
+		for (int i = 0; i < absolute(x2); i++) {
+			if (x2 < 0) {
+				x1++ ;
+			}
+			else {
+				x1-- ;
+			}
 		}
-	}
-				else {
-					variable1 = plus(variable1, variable2);
-				}
-		return variable1;
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		int variable1 = x1; 
-		int variable2 = x2;
-		int multiplication = x1;
-		if (variable1 == 1){
-			return variable2;
+		int multiplication = 0;
+		if (x1 == 0 || x2 ==0) {
+			return 0;
 		}
-			if (variable1 == 0){
-				return 0; 
-			}
-				if (variable2 == 0) {
-					return 0; 
-				}
-					else {
-						for (int i = 1; i < variable2; i++) {
-							multiplication = plus(multiplication, variable1); 
-						} 	
-							return multiplication;
+			if (x2 == 1) {
+			return x1;
+		}
+				if (x1 > 0 && x2 > 0 || x1 < 0 && x2 < 0) {
+					for (int i = 0; i < absolute(x2); i++){
+					multiplication = plus(multiplication, absolute(x1));
 					}
+					return multiplication;
+				}
+						else {
+							for (int i = 0; i < absolute(x2); i++){
+								multiplication = minus(multiplication, absolute(x1));
+								}
+								return multiplication;
+						}
 
-	}
+		}
+
+	
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		int variable = x ;
-		int num = n ;
-		int result = x; 
-		if (num == 1){
-			return variable;
+		if (x == 0 && n == 0) {
+			return (int)Double.NaN;
 		}
-			if (num == 0){
-				return 1;
+		if (x == 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return x;
+		}
+		if (n == 0) {
+			return 1;
+		}
+		if (x == 1) {
+			return 1;
+		}
+		if (x < 0){
+			if (mod(n, 2) == 0){
+			x = absolute(x);
+			int variable = x ;
+			for (int i = 1; i < n; i++) {
+			variable = times(variable, x);
 			}
-				else{
-					for (int i = 1; i < num; i++){
-						result = times(result, variable);
-					}
-					return result;
+			return variable;	
+			}
 
+			else {
+				int variable = x ;
+				for (int i = 1; i < n; i++) {
+				variable = minus(variable, x);
+				}
+				return variable;	
+	
+				}
+			}
+
+			else {
+				int variable = x ;
+				for (int i = 1; i < n; i++) {
+				variable = times(variable, x);
+				}
+				return variable;	
 				}
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		int variable1 = x1;
-		int variable2 = x2;
-		int counter = 0; 
-		if (variable1 == variable2){
-			return 1;
-		} 
-			if (variable1 < variable2){
-				return 0; 
-			}
-				if (variable2 == 1){
-					return variable1;
-				}
-					else {
+		int counter = 0;
 
-						while (variable1 > 0){
-							variable1 = minus(variable1, variable2);
-							if (variable1 >= 0){
-								counter ++; 
-							}
-						}
-						return counter;
+		if (x2 == 0) {
+			return (int)Double.NaN;
+			
+		}
+
+		if (x1 == 0) {
+			return 0; 
+		} 
+		if (x2 == 1) {
+			return x1;
+		}
+
+		if ((x1 > 0 && x2 > 0) || (x1 <0 && x2 <0)) {
+			int variable1 = absolute(x1);
+			int variable2 = absolute(x2);
+			while (variable1 > 0){
+				variable1 = minus(variable1, variable2);
+				if (variable1 >= 0){
+				counter ++; 
 				}
-	}
+					}
+			return counter;
+		}
+
+		else {
+			int variable1 = absolute(x1);
+			int variable2 = absolute(x2);
+			while (variable1 > 0){
+				variable1 = minus(variable1, variable2);
+				if (variable1 >= 0){
+				counter --; 
+				}
+					}
+			return counter;
+
+		}
+
+			}
+	
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		int variable1 = x1;
-		int variable2 = x2; 
-		if (variable1 == variable2){
-			return 0;
-		}
-			if ( variable2 > variable1){
-				return variable1;
-			}
-				else { 
-					int modular = div(variable1, variable2);
-					modular = times(modular, variable2);
-					modular = minus(variable1, modular);
-					return modular;
+		int div = div(x1, x2);
+		int modular = minus(x1, times(x2, div));
+		return modular;
 
-				}
-	}	
+		}
+		
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		int variable = x; 
-		if (x > 0){
-			int i = 1; 
-			while (i <= variable){
-				if (variable == pow(i,2)) {
-					return i;
-				}
-				i++ ;
-			}
+		if (x < 0) {
+			return (int)Double.NaN; 
 		}
-		return 0;
+		int result = 0;
+		while (times(result, result) <= x) {
+			result ++;
+		}
+		return minus(result, 1);
 	}	  	  
 }
