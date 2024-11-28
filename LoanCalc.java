747f7e61a3ed7d333393ed5759fb5abdc76c43a0
NoaOz <noa.oz01@post.runi.ac.il>
2024-11-28 23:57:44 +0200

    completed loan try

--- LoanCalc.java
+++ LoanCalc.java
@@ -28,8 +28,11 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		rate = rate / 100;
+		for (int i =0; i < n; i++) {
+			loan = (loan - payment) * (1 + rate);
+		}
+		return loan;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +41,18 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		int counter = 0;
+		double pay = loan / n;
+		double increment = epsilon;
+		while (true) {
+			double balance =endBalance(loan, rate, n, pay);
+				if (balance <= 0){
+					break;
+				}
+				pay = pay + increment;
+				counter ++;
+		}
+		return pay;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -48,7 +61,28 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+        int counter =0;
+		double low = loan/ n;
+		double high = loan;
+		double amid = 0.0;
+		 
+		while ((high - low) > epsilon) {
+			amid = (low + high) / 2;
+			double balance = endBalance(loan, rate, n, amid);
+			if (balance > epsilon) {
+				low = amid;
+			}
+			else {
+				if (balance < (epsilon*(-1))) {
+					high = amid;
+				}
+				else {
+					return amid;
+				}
+				counter ++;
+			}
+		}
+		return (low + high) / 2;
     }
+
 }
